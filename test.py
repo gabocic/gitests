a3abac248c528c1c6af6df2090148745d0e2d32c
Gabriel Ciciliani <gabriel.ciciliani@gmail.com>
2018-12-19 22:44:56 -0300

    adding do_concat and test2.py

--- test.py
+++ test.py
@@ -8,6 +8,10 @@ def do_something_else(p1,p2):
     c = int(p1) + int(p2)
     print(c)
 
+def do_concat(p1,p2):
+    print(p1+' '+p2)
+
 if __name__ == '__main__':
     do_something(4,'hello')
     do_something_else(5,6)
+    do_concat('hello','world')
