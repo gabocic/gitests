953aef02e554d0fc6d1349ca7971a6d47747a20e
Gabriel Ciciliani <gabriel.ciciliani@gmail.com>
2018-12-20 18:21:57 -0300

    changing parameters in test3

--- test3.py
+++ test3.py
@@ -12,6 +12,6 @@ def do_concat(p1,p2):
     print(p1+' '+p2)
 
 if __name__ == '__main__':
-    do_something(4,'hello')
-    do_something_else(5,6)
-    do_concat('hello','world')
+    do_something(4,'hello world')
+    do_something_else(50,60)
+    do_concat('foo','bar')
