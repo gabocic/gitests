dd30c2dfcefa2d1daff17b7b7cf6c5740baca31e
Gabriel Ciciliani <gabriel.ciciliani@gmail.com>
2018-12-19 22:41:57 -0300

    adding do_something_else

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ def do_something(p1,p2):
     print(type(p1))
     print(type(p2))
 
+def do_something_else(p1,p2):
+    c = int(p1) + int(p2)
+    print(c)
 
 if __name__ == '__main__':
     do_something(4,'hello')
+    do_something_else(5,6)
