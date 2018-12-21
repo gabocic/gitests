014e6ed44fd17f76bcf7d0f90ef1b4e134cae80f
Gabriel Ciciliani <gabriel.ciciliani@gmail.com>
2018-12-20 23:41:00 -0300

    changing space for dash in do_concat (test) and adding foo-bar in test2

--- test.py
+++ test.py
@@ -9,9 +9,10 @@ def do_something_else(p1,p2):
     print(c)
 
 def do_concat(p1,p2):
-    print(p1+' '+p2)
+    print(p1+'-'+p2)
 
 if __name__ == '__main__':
     do_something(4,'hello')
     do_something_else(5,6)
     do_concat('hello','world')
+    do_concat('foo','bar')
--- test2.py
+++ test2.py
@@ -3,3 +3,4 @@
 
 if __name__ == '__main__':
     print('empty')
+    print('not empty')
