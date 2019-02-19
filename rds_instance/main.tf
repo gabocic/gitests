45979f96aa9bbed96fb3e5748777e106666ac517
Gabriel Ciciliani <gabriel.ciciliani@gmail.com>
2019-02-18 22:23:51 -0300

    Moving main.tf inside rds_instance

--- rds_instance/main.tf
+++ rds_instance/main.tf
@@ -1,4 +1,5 @@
-resource "aws_db_instance" "default" {
+resource "aws_db_instance" "master" {
+  identifier           = "tf-rds-poc"
   skip_final_snapshot  = true
   allocated_storage    = 5
   storage_type         = "gp2"
@@ -12,15 +13,11 @@ resource "aws_db_instance" "default" {
 }
 
 resource "aws_db_instance" "replica" {
+  identifier           = "tf-rds-poc-replica"
   skip_final_snapshot  = true
-  allocated_storage    = 5
-  storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  name                 = "mydb-replica"
-  username             = "foo"
-  password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
-  replicate_source_db  = "mydb"
+  replicate_source_db  = "tf-rds-poc"
 }
