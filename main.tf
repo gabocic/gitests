4b18b16dc2f1b287e4a1285fdd06ee8b5ab96ae8
Gabriel Ciciliani <gabriel.ciciliani@gmail.com>
2019-02-18 21:49:49 -0300

    Adding read-replica

--- main.tf
+++ main.tf
@@ -10,3 +10,17 @@ resource "aws_db_instance" "default" {
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
 }
+
+resource "aws_db_instance" "default" {
+  skip_final_snapshot  = true
+  allocated_storage    = 5
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb-replica"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  replicate_source_db  = "mydb"
+}
