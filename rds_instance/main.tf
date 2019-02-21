6c939c7197a0a25c0c47e13d246d29b702d63442
Gabriel Ciciliani <gabriel.ciciliani@gmail.com>
2019-02-21 12:10:04 -0300

    Adding resources for AWS sg and MySQL users

--- rds_instance/main.tf
+++ rds_instance/main.tf
@@ -1,23 +1,74 @@
+resource "aws_security_group" "instance_sg" {
+  name        = "${var.instance_name}"
+  description = "allow connections on port 3306"
+  vpc_id      = "${var.vpcid}"
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
 resource "aws_db_instance" "master" {
-  identifier           = "tf-rds-poc"
-  skip_final_snapshot  = true
-  allocated_storage    = 5
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  name                 = "mydb"
-  username             = "foo"
-  password             = "foobarbaz"
-  parameter_group_name = "default.mysql5.7"
+  identifier           		= "${var.instance_name}"
+  skip_final_snapshot  		= true
+  allocated_storage    		= 5
+  storage_type         		= "gp2"
+  engine               		= "mysql"
+  engine_version       		= "5.7"
+  instance_class       		= "db.t2.micro"
+  username             		= "foo"
+  password             		= "foobarbaz"
+  vpc_security_group_ids	= ["${aws_security_group.instance_sg.*.id}"]
+  tags                 		= {
+                            	environment = "${var.instance_name}"
+                         	}
 }
 
 resource "aws_db_instance" "replica" {
-  identifier           = "tf-rds-poc-replica"
+  identifier           = "${var.instance_name}-replica"
   skip_final_snapshot  = true
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  parameter_group_name = "default.mysql5.7"
-  replicate_source_db  = "tf-rds-poc"
+  replicate_source_db  = "${var.instance_name}"
+}
+
+provider "mysql" {
+    endpoint = "${aws_db_instance.master.endpoint}"
+    username = "${var.admin_username}"
+    password = "${var.admin_password}"
+}
+
+resource "mysql_user" "ro_user" {
+  user               = "ro_user"
+  host               = "%"
+  plaintext_password = "S3Cre7pAss!"
+}
+
+resource "mysql_grant" "jdoe" {
+  user       = "${mysql_user.ro_user.user}"
+  host       = "${mysql_user.ro_user.host}"
+  database   = "${var.instance_name}"
+  privileges = ["SELECT"]
+}
+
+resource "mysql_user" "rw_user" {
+  user               = "rw_user"
+  host               = "%"
+  plaintext_password = "S3Cre7pAss!"
+}
+
+resource "mysql_grant" "rw_user" {
+  user       = "${mysql_user.rw_user.user}"
+  host       = "${mysql_user.rw_user.host}"
+  database   = "${var.instance_name}"
+  privileges = ["SELECT","UPDATE","DELETE","INSERT"]
+}
+
+output "endpoint" {
+  value = "${aws_db_instance.master.endpoint}"
 }
